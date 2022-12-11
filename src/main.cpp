5dc0e2a2c15754efd8fd2a382d3d9e712727ede7
farisrfp <farisrafi2903@gmail.com>
2022-12-11 18:51:27 +0700

    docs : nyoba gps udah aman

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,23 @@
 #include <Arduino.h>
-#include "creds.h"
-#define ESP8266
+#include <TinyGPSPlus.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-uint8_t temprature_sens_read();
-#ifdef __cplusplus
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+void setup(){
+  Serial.begin(9600);
+  Serial2.begin(9600);
 }
-#endif
-uint8_t temprature_sens_read();
 
-#ifdef ESP8266
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
-#else
-#include <WiFi.h>
-#include <HTTPClient.h>
-#endif
-#include <PubSubClient.h>
-
-
-// Add your MQTT Broker IP address, example:
-const char *mqtt_broker = "broker.emqx.io";
-const char *topic = "esp32/testTemp";
-const char *mqtt_username = "emqx";
-const char *mqtt_password = "public";
-const int mqtt_port = 1883;
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-void setup() {
-  // Set software serial baud to 115200;
-  Serial.begin(115200);
-
-  // connecting to a WiFi network
-  WiFi.begin(SSID, PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
-  }
-  Serial.println("Connected to the WiFi network");
-
-  //connecting to a mqtt broker
-  client.setServer(mqtt_broker, mqtt_port);
-  while (!client.connected()) {
-    String client_id = "esp32testClient-";
-    client_id += String(WiFi.macAddress());
-    Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
-    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
-      Serial.println("Public emqx mqtt broker connected");
-    } else {
-      Serial.print("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
+void loop(){
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (Serial2.available() > 0){
+    gps.encode(Serial2.read());
+    if (gps.location.isUpdated()){
+      Serial.print("Latitude= "); 
+      Serial.print(gps.location.lat(), 6);
+      Serial.print(" Longitude= "); 
+      Serial.println(gps.location.lng(), 6);
     }
   }
 }
-
-void loop() {
-  // float temp = (temprature_sens_read() - 32) / 1.8;
-  float temp = random(20, 40);
-
-  char temps[10];
-  sprintf(temps,"%3.2f", temp);
-  client.publish(topic, temps);
-  client.loop();
-
-  delay(1000);
-}
-
